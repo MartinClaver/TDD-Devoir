5ecb8d80162fe9735e75c016d2a3ea00ce5b97f9
mclav <m.claver99@gmail.com>
2026-02-12 16:58:23 +0100

    one pair test to green

--- src/main/poker/HandEvaluator.java
+++ src/main/poker/HandEvaluator.java
@@ -11,8 +11,25 @@ public class HandEvaluator {
         allCards.addAll(board);
         allCards.sort((c1, c2) -> Integer.compare(c2.getRank().value, c1.getRank().value));
         List<Card> best5 = allCards.subList(0, 5);
+
         Map<Rank, Long> counts = allCards.stream()
                 .collect(Collectors.groupingBy(Card::getRank, Collectors.counting()));
+
+        Rank pairRank = null;
+
+        for (Map.Entry<Rank, Long> entry : counts.entrySet()) {
+            if (entry.getValue() == 2) {
+                pairRank = entry.getKey();
+                break;
+            }
+        }
+
+        if (pairRank != null) {
+            // C'est ici que tu devras construire ta liste de 5 cartes
+            // (Les 2 cartes de la paire + les 3 meilleures autres)
+            return new BestHand(HandCategory.PAIR, best5);
+        }
+
         return new BestHand(HandCategory.HIGH_CARD, best5);
     }
 }
